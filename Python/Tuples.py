e4e420e5e6ba524360762c6116a2561dcfe5adbc
DonaldoAG99999 <donaldhereicih@hotmail.com>
2023-04-03 22:45:09 -0600

    03-04-2023 Challenges

---
+++ Python/Tuples.py
@@ -0,0 +1,14 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+integers = list(input())
+clean_list = []
+
+for integer in integers:
+    if (integer != ' '):
+        clean_list.append(int(integer))
+        
+t = tuple(clean_list)
+
+h = hash(t)
+
+print(h)
